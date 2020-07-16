d44c2c75fe42859eb84fc66761fe54290f657305
Lars Löffler <lars.loeffler@infodesire.com>
2020-07-16 22:13:35 +0200

    first tabular test running

--- src/main/de/lamosoft/bosstest/SimpleCalls.java
+++ src/main/de/lamosoft/bosstest/SimpleCalls.java
@@ -1,10 +1,22 @@
 package de.lamosoft.bosstest;
 
+import java.util.Arrays;
 import java.util.function.Function;
+
+import static org.junit.Assert.assertEquals;
 
 public class SimpleCalls {
 
   public void test( String[][] params, Function<String, String> f ) {
+
+    Arrays.stream( params ).forEach( line -> {
+
+      String expected = line[ 0 ];
+      String param = line[ 1 ];
+      String actual = f.apply( param );
+      assertEquals( expected, actual );
+
+    });
 
   }
 
